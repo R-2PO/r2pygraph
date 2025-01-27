a4324b694c93b49d33c605817644670c7bb79599
R-2PO <95053976+R-2PO@users.noreply.github.com>
2025-01-27 21:06:10 +0100

    Change the default window size and add the drawing canvas

--- __init__.py
+++ __init__.py
@@ -2,9 +2,11 @@ import tkinter
 
 
 class Window (tkinter.Tk):
-    def __init__ (self, title: str="Window", geometry: str="800x450"):
+    def __init__ (self, title: str="Window", geometry: str="960x540"):
         super().__init__()
         self.title(title)
+        self.figure_canvas = tkinter.Canvas(master=self, highlightthickness=0, border=0)
+        self.figure_canvas.pack(fill="both", expand=True)
         self.geometry(geometry)
 
     def show (self):
